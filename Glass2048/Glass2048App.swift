457671be31ab2c3072069832d24dd905a9412b6e
Jacob Parker <jacobparker1992@gmail.com>
2025-10-25 13:51:04 +0100

    Initial Commit

---
+++ Glass2048/Glass2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Glass2048App.swift
+//  Glass2048
+//
+//  Created by Jacob Parker on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Glass2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
